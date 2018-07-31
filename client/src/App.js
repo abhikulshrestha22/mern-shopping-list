cfdfae83d1bda4b60862ade7d358a62e9475893c
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2018-07-31 17:08:24 +0530

    redux and react-modal added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import AppNavBar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ItemModal from './components/ItemModal';
+import {Container} from 'reactstrap';
+
+import {Provider} from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <AppNavBar />
+        <Container>
+        <ItemModal/>
+        <ShoppingList/>
+        </Container>
+        
+
       </div>
+      </Provider>
     );
   }
 }
---
+++ client/src/store.js
@@ -0,0 +1,22 @@
+import {createStore, applyMiddleware, compose}  from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+// eslint-disable-next-line no-underscore-dangle
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
+window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+    devTools = a => a;
+}
+
+
+const initialState={};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer,initialState, compose(
+    applyMiddleware(...middleware),
+    devTools
+));
+
+export default store;
